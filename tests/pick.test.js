33d6bd87d17d7f38948d8a25652d835dc94bc2b9
valeriocipolla <valeriocipolla92@gmail.com>
2022-04-12 16:54:01 +0100

    test

--- tests/pick.test.js
+++ tests/pick.test.js
@@ -3,5 +3,5 @@ import { pick } from "../lib/pick.js";
 it("returns a random item from an array", () => {
   const arr = [1, 2, 3, 4, 5, "hello", true, false];
   const actual = pick(arr);
-  expect(arr.includes(actual)).toBeTruthy();
+  expect(arr.includes(actual)).toBeFalsy();
 });
