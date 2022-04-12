85536a89ab3d05ec955088cd2f32b8df0f264fcb
valeriocipolla <valeriocipolla92@gmail.com>
2022-04-12 17:14:19 +0100

    pick test

--- tests/pick.test.js
+++ tests/pick.test.js
@@ -5,3 +5,11 @@ it("returns a random item from an array", () => {
   const actual = pick(arr);
   expect(arr.includes(actual)).toBeTruthy();
 });
+
+it("throws an error when called with not an array", () => {
+  const str = "Hello";
+  function pickFromStr() {
+    pick(str);
+  }
+  expect(pickFromStr).toThrowError(TypeError(`${str} is not an Array`));
+});
