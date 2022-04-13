8c14cf1c49248870460e52a55ad705298aa900d2
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2022-04-13 22:30:10 +0100

    Adding more specific tests for 'int'

--- tests/int.test.js
+++ tests/int.test.js
@@ -10,19 +10,34 @@ describe("int", () => {
     expect(actual).toBeLessThan(max);
   });
 
+  it.each([
+    { mockValue: 0.412, min: 0, max: 3, expected: 1 },
+    { mockValue: 0.15029, min: 416, max: 5_905, expected: 1_240 },
+    { mockValue: 0.5602, min: 15, max: 67, expected: 44 },
+    { mockValue: 0.1294, min: 1502, max: 53_047, expected: 8_171 },
+    { mockValue: 0.37656, min: 16, max: 23, expected: 18 },
+    { mockValue: 0.9713, min: 160_590, max: 1_250_858, expected: 1_219_567 },
+  ])(
+    "int($min, $max) should return $expected when Math.random returns $mockValue",
+    ({ mockValue, min, max, decimals, expected }) => {
+      jest.spyOn(Math, "random").mockReturnValueOnce(mockValue);
+
+      const actual = int(min, max, decimals);
+      expect(actual).toBe(expected);
+
+      Math.random.mockRestore();
+    },
+  );
+
   it("throws an error if arguments are not both integers", () => {
     const min = "hello";
     const max = 3;
-    expect(() => int(min, max)).toThrow(
-      TypeError("Both arguments need to be integers")
-    );
+    expect(() => int(min, max)).toThrow(TypeError("Both arguments need to be integers"));
   });
 
   it("throws an error if min is greater or equal to max", () => {
     const min = 10;
     const max = 1;
-    expect(() => int(min, max)).toThrow(
-      Error("first argument needs to be smaller than second argument")
-    );
+    expect(() => int(min, max)).toThrow(Error("first argument needs to be smaller than second argument"));
   });
 });
