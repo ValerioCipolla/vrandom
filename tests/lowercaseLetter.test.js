c5b7a49f4a41aa51cedcecb6406512ec20256150
kennarddh <kennarddh@gmail.com>
2022-04-15 14:05:08 +0700

    test: Random lowercase letter

---
+++ tests/lowercaseLetter.test.js
@@ -0,0 +1,22 @@
+const lowercaseLetter = require("../lib/lowercaseLetter.js");
+
+describe("lowercase letter", () => {
+  it("should return a random lowercase letter", () => {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    const letter = lowercaseLetter();
+
+    expect(lowerAlphabet.includes(letter)).toBe(true);
+  });
+
+  it("returns a string", () => {
+    const actual = lowercaseLetter();
+
+    expect(typeof actual).toBe("string");
+  });
+
+  it("returns a string of lenght 1", () => {
+    const actual = lowercaseLetter();
+    
+    expect(actual.length).toBe(1);
+  });
+});
