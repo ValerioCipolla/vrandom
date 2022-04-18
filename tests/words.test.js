f6c25be80c92e66cdc4d732c80dc314f77996ae4
valeriocipolla <valeriocipolla92@gmail.com>
2022-04-18 10:11:37 +0100

    words method and basic tests created

---
+++ tests/words.test.js
@@ -0,0 +1,82 @@
+const words = require("../lib/words.js");
+
+describe("words", () => {
+  it("returns an error if called without arguments", () => {
+    expect(() => words()).toThrow(TypeError("First argument needs to be a positive integer"));
+  });
+
+  it("returns an error if called with a string size argument", () => {
+    expect(() => words("hello")).toThrow(TypeError("First argument needs to be a positive integer"));
+  });
+
+  it("returns an error if called with a negative integer size argument", () => {
+    expect(() => words(-4)).toThrow("First argument needs to be a positive integer");
+  });
+
+  it.each([
+    { size: 5, format: 1 },
+    { size: 5, format: "hello" },
+    { size: 5, format: true },
+    { size: 5, format: false },
+    { size: 5, format: 12434 },
+  ])("returns an error if called with invalid format argument '$format'", ({ size, format }) => {
+    expect(() => words(size, format)).toThrow(Error(`Invalid format '${format}' picked`));
+  });
+
+  it("returns an array of lowercase words when called with 1 argument", () => {
+    const size = 5;
+    const actual = words(size);
+    expect(typeof actual).toBe("object");
+    actual.forEach(w => {
+      expect(typeof w).toBe("string");
+      expect(w).toBe(w.toLowerCase());
+    });
+  });
+
+  it.each([{ size: 2 }, { size: 5 }, { size: 10 }, { size: 25 }, { size: 100 }, { size: 1000 }])(
+    "words($size) returns an array of size $size of lowercase words",
+    ({ size }) => {
+      const actual = words(size);
+      expect(typeof actual).toBe("object");
+      expect(actual.length).toBe(size);
+      actual.forEach(w => {
+        expect(typeof w).toBe("string");
+        expect(w).toBe(w.toLowerCase());
+      });
+    },
+  );
+
+  it.each([
+    { size: 2, format: "uppercase" },
+    { size: 5, format: "uppercase" },
+    { size: 10, format: "uppercase" },
+    { size: 25, format: "uppercase" },
+    { size: 100, format: "uppercase" },
+    { size: 1000, format: "uppercase" },
+  ])("words($size, $format) returns an array of size $size of uppecase words", ({ size, format }) => {
+    const actual = words(size, format);
+    expect(typeof actual).toBe("object");
+    expect(actual.length).toBe(size);
+    actual.forEach(w => {
+      expect(typeof w).toBe("string");
+      expect(w).toBe(w.toUpperCase());
+    });
+  });
+
+  it.each([
+    { size: 2, format: "capitalized" },
+    { size: 5, format: "capitalized" },
+    { size: 10, format: "capitalized" },
+    { size: 25, format: "capitalized" },
+    { size: 100, format: "capitalized" },
+    { size: 1000, format: "capitalized" },
+  ])("words($size, $format) returns an array of size $size of capitalized words", ({ size, format }) => {
+    const actual = words(size, format);
+    expect(typeof actual).toBe("object");
+    expect(actual.length).toBe(size);
+    actual.forEach(w => {
+      expect(typeof w).toBe("string");
+      expect(w).toBe(w[0].toUpperCase() + w.slice(1).toLowerCase());
+    });
+  });
+});
